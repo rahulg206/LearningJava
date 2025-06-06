6013200c35ef88ee527e2f23590a0f49613cb85e
rahulg206 <rahulg206@gmail.com>
2025-06-06 09:31:20 +0530

    code added

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,10 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Rahul");
+        System.out.println("Hello Rahul");
+        System.out.println("Hello Rahul");
+        System.out.println("Hello Rahul");
+        System.out.println("Hello Rahul");
+
     }
 }
