5cefcbbdc92fa2ddf5c380b4b8a5b5141220a048
rahulg206 <rahulg206@gmail.com>
2025-06-11 01:14:57 +0530

    Variables Literals code added

---
+++ src/ex_02_Java_Basics/Lab_021_Boolean.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_021_Boolean {
+    public static void main(String[] args) {
+        boolean is_rahul_married = true;
+        boolean is_rahul_married1 = false;
+        boolean rahul_has_audi = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+    }
+}
