35f4dcec4fcea1cdf7588c8988890879ecd9c5c9
rahulg206 <rahulg206@gmail.com>
2025-06-10 06:33:55 +0530

    code added

---
+++ src/Lab002_Comments.java
@@ -0,0 +1,6 @@
+public class Lab002_Comments {
+    public static void main(String[] args) {
+       // This is comment , this will not be executed
+        System.out.println("Hi");
+    }
+}
---
+++ src/Lab003_Comments_Multi.java
@@ -0,0 +1,10 @@
+public class Lab003_Comments_Multi {
+    public static void main(String[] args) {
+        /*
+        *Author : Rahul Gunjal
+        *Course : ATB13x
+        *
+        */
+        System.out.println("Hi Multi Comment");
+    }
+}
