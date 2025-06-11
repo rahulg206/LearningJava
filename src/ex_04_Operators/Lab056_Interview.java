9d5d00e79b6590c70f96756079375655a008741d
rahulg206 <rahulg206@gmail.com>
2025-06-12 01:10:52 +0530

    Operators 11thJune code added

---
+++ src/ex_04_Operators/Lab056_Interview.java
@@ -0,0 +1,16 @@
+package ex_04_Operators;
+
+import java.net.SocketTimeoutException;
+
+public class Lab056_Interview {
+    public static void main(String[] args){
+        short s = 10;
+        char c = 'A';
+        String s1 = "A";
+        System.out.println(c+s);
+        System.out.println(c);
+        System.out.println(c+c);
+        System.out.println(c+s1);
+
+    }
+}
